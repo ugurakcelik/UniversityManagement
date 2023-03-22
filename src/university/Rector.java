f422de091569333b28c41fab46fb0483ee10a41b
ugurakcelik <57275553+ugurakcelik@users.noreply.github.com>
2023-03-22 01:49:46 +0100

    v.0.0

---
+++ src/university/Rector.java
@@ -0,0 +1,28 @@
+package university;
+
+public class Rector {
+	
+	private String first;
+	private String last;
+	
+	public String getFirst() {
+		return first;
+	}
+	
+	public void setFirst(String first) {
+		this.first = first;
+	}
+
+	public String getLast() {
+		return last;
+	}
+
+	public void setLast(String last) {
+		this.last = last;
+	}
+	
+	@Override
+	public String toString() {
+		return getFirst() + " " + getFirst();
+	}	
+}
