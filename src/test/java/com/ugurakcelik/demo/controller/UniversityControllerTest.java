4d6a4a2a8b481d2defec8da5ca68aab76f07eb8b
ugurakcelik <ugurakcelik10@gmail.com>
2023-04-18 03:35:11 +0200

    Unit tests

---
+++ src/test/java/com/ugurakcelik/demo/controller/UniversityControllerTest.java
@@ -0,0 +1,71 @@
+package com.ugurakcelik.demo.controller;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class UniversityControllerTest {
+
+    //Create tests for all methods in UniversityController
+
+    @Test
+    void createUniversity() {
+
+    }
+
+    @Test
+    void getUniversity() {
+    }
+
+    @Test
+    void setRector() {
+    }
+
+    @Test
+    void getRector() {
+    }
+
+    @Test
+    void enrollStudent() {
+    }
+
+    @Test
+    void getStudent() {
+    }
+
+    @Test
+    void activateCourse() {
+    }
+
+    @Test
+    void getCourse() {
+    }
+
+    @Test
+    void registerStudentToCourse() {
+    }
+
+    @Test
+    void getCourseAttendees() {
+    }
+
+    @Test
+    void getStudentStudyPlan() {
+    }
+
+    @Test
+    void addExamGrade() {
+    }
+
+    @Test
+    void getStudentAverage() {
+    }
+
+    @Test
+    void getCourseAverage() {
+    }
+
+    @Test
+    void getTopThreeStudents() {
+    }
+}
